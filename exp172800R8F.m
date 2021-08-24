01390ee3772c2de7d0bf86ae6afeda66c5249bb7
MahashwetaPatra <89363530+MahashwetaPatra@users.noreply.github.com>
2021-08-23 22:23:49 -0400

    Add files via upload

---
+++ exp172800R8F.m
@@ -0,0 +1,41 @@
+%
+% NOTE: This is for calculating variance and error growth from cm1 model when Noise is introduced at 172800
+%
+% HIST:  - 13 Jan, 2021: Created by Patra
+%=========================================================================
+function EB8 = exp172800R8F
+T1=1:1:24001; %Time steps
+T=T1*30/3600; % converting time to hours
+T2=1:1:24001; %Time steps
+T3=T2*36/3600; % converting time to hours
+sum=0;% initialization for the error growth
+S=[];tao=4800;delta=600;
+V = importdata('exp172800R8Fv.txt');%Time series with zero noise amplitude
+for i=1: 28 % number of realization
+    filename = sprintf('exp172800R8Fv%d.txt',i);% loading the v-wind data for all realization
+    Vmax(:,i)= load(filename);
+%     figure(1);
+%     set(gca, 'GridLineStyle', ':') %dotted grid lines
+%     set(gca,'FontSize',14,'LineWidth',2.75)
+%     plot(T3,Vmax(:,i),'.', 'markersize',2)% Plotting the time series for all realization
+%     hold on;
+%     xlabel('Time / Hours');
+%     ylabel('Vmax')
+    v1=Vmax(:,i);
+    S=[S;std(v1(10700:17801))];
+    sum=sum+(((v1(tao+delta)-V(tao+delta))/delta)*(((v1(tao+delta)-V(tao+delta))/delta)));% summing up for all realization
+end
+MS=mean(S);
+EG=sqrt(sum/i);% calculating the error growth
+%   hold on;
+%   plot(T3,V,  '.black','markersize',8)% Plotting the time series for zero noise amplitude
+%   hold off;
+P=mean(S);
+SF=std(S);
+ZF=1.96;
+nF=sqrt(30);
+P1=P+(ZF*SF)/sqrt(30);
+P2=P-(ZF*SF)/sqrt(30);
+EB8=[P, P1, P2];
+end
+
