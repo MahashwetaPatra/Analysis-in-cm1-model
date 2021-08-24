b9b866eb4415413ae686016dd53614aac6087f24
MahashwetaPatra <89363530+MahashwetaPatra@users.noreply.github.com>
2021-08-23 22:18:51 -0400

    Add files via upload

---
+++ STDRI.m
@@ -0,0 +1,26 @@
+clc; close all; clear all;
+Noise=[];EB=[];tau=172800;
+EB1 = exp172800RI;  Noise1=[0.01;0.01;0.01]; 
+plot(Noise1,EB1,'-red', 'markersize',20)
+hold on;
+EB2 = exp172800R2I; Noise2=[0.02;0.02;0.02];
+plot(Noise2,EB2,'-red', 'markersize',20)
+EB3 = exp172800R3I; Noise3=[0.03;0.03;0.03];
+plot(Noise3,EB3,'-red', 'markersize',20)
+EB4 = exp172800R4I;  Noise4=[0.04;0.04;0.04];
+plot(Noise4,EB4,'-red', 'markersize',20)
+EB5 = exp172800R5I; Noise5=[0.05;0.05;0.05];
+plot(Noise5,EB5,'-red', 'markersize',20)
+EB6 = exp172800R6I; Noise6=[0.06;0.06;0.06];
+plot(Noise6,EB6,'-red', 'markersize',20)
+EB7 = exp172800R7I; Noise7=[0.07;0.07;0.07];
+plot(Noise7,EB7,'-red', 'markersize',20)
+EB8 = exp172800R8I; Noise8=[0.08;0.08;0.08];
+plot(Noise8,EB8,'-red', 'markersize',20)
+EB9 = exp172800R9I;Noise9=[0.09;0.09;0.09];
+plot(Noise9,EB9,'-red', 'markersize',20)
+Noise=[Noise1(1);Noise2(1);Noise3(1);Noise4(1);Noise5(1);Noise6(1);Noise7(1);Noise8(1);Noise9(1);];
+EB=[EB1(1);EB2(1);EB3(1);EB4(1);EB5(1);EB6(1);EB7(1);EB8(1);EB9(1);];
+plot(Noise,EB,'.-black', 'markersize',20)% Plotting the time series for all realization
+xlabel('Noise amplitude'); 
+ylabel('Standard deviation'); 
---
+++ exp172800R2I.m
@@ -0,0 +1,41 @@
+%
+% NOTE: This is for calculating variance and error growth from cm1 model when Noise is introduced at 172800
+%
+% HIST:  - 13 Jan, 2021: Created by Patra
+%=========================================================================
+
+function EB2 = exp172800R2I
+T1=1:1:24001; %Time steps
+T=T1*30/3600; % converting time to hours
+T2=1:1:24001; %Time steps
+T3=T2*36/3600; % converting time to hours
+sum=0;% initialization for the error growth
+S=[];tao=4800;delta=600;
+%V = importdata('out_v.txt');%Time series with zero noise amplitude
+for i=1: 30 % number of realization
+    filename = sprintf('exp172800R2Iv%d.txt',i);% loading the v-wind data for all realization
+    Vmax(:,i)= load(filename);
+%     figure(1);
+%     set(gca, 'GridLineStyle', ':') %dotted grid lines
+%     set(gca,'FontSize',14,'LineWidth',2.75)
+%     plot(T3,Vmax(:,i),'.', 'markersize',2)% Plotting the time series for all realization
+%     hold on;
+%     xlabel('Time / Hours');
+%     ylabel('Vmax')
+    v1=Vmax(:,i);
+    S=[S;std(v1(11500:14001))];
+  %  sum=sum+(((v1(tao+delta)-V(tao+delta))/delta)*(((v1(tao+delta)-V(tao+delta))/delta)));% summing up for all realization
+end
+MS=mean(S);
+%EG=sqrt(sum/i)% calculating the error growth
+%hold on;
+%plot(T3,V,  '.black','markersize',8)% Plotting the time series for zero noise amplitude
+%hold off;
+P=mean(S);
+SF=std(S);
+ZF=1.96;
+nF=30;
+P1=P+(ZF*SF)/sqrt(30);
+P2=P-(ZF*SF)/sqrt(30);
+EB2=[P, P1, P2];
+end
